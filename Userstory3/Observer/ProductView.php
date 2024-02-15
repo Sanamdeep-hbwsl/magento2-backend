24a8ae9bf1d3dd546d5317548371c62d02173b10
Sanamdeep-hbwsl <sanamdeep.bhatia@hbwsl.com>
2024-02-15 11:04:52 +0530

    userstory 3

---
+++ Userstory3/Observer/ProductView.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sanamdeep\Userstory3\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Psr\Log\LoggerInterface;
+
+class ProductView implements ObserverInterface{
+
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function execute(Observer $observer){
+        $product = $observer->getProduct();
+        $originalName = $product->getName();
+
+        $modifiedName = $originalName . ' -Modified by Sanamdeep with Observer and events';
+
+        $product->setName($modifiedName);
+        
+        $this->logger->info('Product Name by Sanamdeep: ' . $originalName);
+
+        return $product;
+    }
+}
