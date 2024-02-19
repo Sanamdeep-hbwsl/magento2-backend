00d74c645a56f2787c89fb2e6f520ad162452fb9
Sanamdeep-hbwsl <sanamdeep.bhatia@hbwsl.com>
2024-02-19 18:59:26 +0530

    Userstory16

---
+++ Userstory16/Block/Adminhtml/System/Config/Form/Field/ColorPicker.php
@@ -0,0 +1,39 @@
+<?php
+namespace Sanamdeep\Userstory16\Block\Adminhtml\System\Config\Form\Field;
+
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class ColorPicker extends Field
+{
+    /**
+     * Get the color picker HTML code.
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $html = $element->getElementHtml();
+        $value = $element->getData('value');
+
+        $html .= '<script>
+            require(["jquery", "jquery/colorpicker"], function ($) {
+                $(document).ready(function () {
+                    var $el = $("#' . $element->getHtmlId() . '");
+                    $el.css("backgroundColor", "'. $value .'");
+
+                    // Attach the color picker
+                    $el.ColorPicker({
+                        color: "'. $value .'",
+                        onChange: function (hsb, hex, rgb) {
+                            $el.css("backgroundColor", "#" + hex).val("#" + hex);
+                        }
+                    });
+                });
+            });
+            </script>';
+
+        return $html;
+    }
+}
---
+++ Userstory16/Controller/Index/Index.php
@@ -0,0 +1,13 @@
+<?php
+// app/code/YourVendor/YourModule/Controller/Index/Index.php
+
+namespace Sanamdeep\Userstory16\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+class Index extends Action
+{
+    public function execute()
+    {
+        die('dsdsfsds');
+    }
+}
