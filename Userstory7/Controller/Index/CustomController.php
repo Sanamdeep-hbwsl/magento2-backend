9999fb1d32c4d2e30b82eb39ced6295d51ebe171
Sanamdeep-hbwsl <sanamdeep.bhatia@hbwsl.com>
2024-02-21 15:32:50 +0530

    userstory7

--- Userstory7/Controller/Index/CustomController.php
+++ Userstory7/Controller/Index/CustomController.php
@@ -1,6 +1,5 @@
 <?php
 namespace Sanamdeep\Userstory7\Controller\Index;
-
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
