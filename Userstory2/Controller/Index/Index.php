1d26725dcc47d347acf7bbb62f0df05d9d3d975a
Sanamdeep-hbwsl <sanamdeep.bhatia@hbwsl.com>
2024-02-21 15:28:29 +0530

    userstory2

--- Userstory2/Controller/Index/Index.php
+++ Userstory2/Controller/Index/Index.php
@@ -3,13 +3,11 @@
 namespace Sanamdeep\Userstory2\Controller\Index;
 
 use Magento\Framework\App\ActionInterface;
-use Magento\Framework\Controller\Result\RawFactory;
 
 class Index implements ActionInterface
 {
     public function execute()
     {
         die('Example');
-        // return $this->resultFactory->create()->setContents('Example---');
     }
 }
